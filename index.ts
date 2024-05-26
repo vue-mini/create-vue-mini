1991059ac15538c3d2439c2005492985462f18fa
Yang Mingshan <y.mingshan3@gmail.com>
2024-05-26 17:28:32 +0800

    docs: update readme

--- index.ts
+++ index.ts
@@ -179,7 +179,9 @@ function generateReadme({
 >) {
   let readme = `# ${projectName}
 
-⚠️ 注意：将此项目导入微信小程序开发者工具时请导入项目根目录而非 \`dist\` 目录。
+⚠️ 注意：将此项目导入微信开发者工具时请选择项目根目录而非 \`dist\` 目录。
+
+更多信息请访问官方文档：[vuemini.org](https://vuemini.org)
 
 ## 依赖安装
 
