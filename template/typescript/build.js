7437d6a8fd2b4b588402628aa90e50328449cbef
Yang Mingshan <y.mingshan3@gmail.com>
2024-11-08 01:38:43 +0800

    feat: remove `promise-polyfill`

--- template/typescript/build.js
+++ template/typescript/build.js
@@ -166,22 +166,6 @@ async function processScript(filePath) {
     return;
   }
 
-  if (filePath.endsWith('app.ts')) {
-    /**
-     * IOS 小程序 Promise 使用的内置的 Polyfill，但这个 Polyfill 有 Bug 且功能不全，
-     * 在某些情况下 Promise 回调不会执行，并且不支持 Promise.prototype.finally。
-     * 这里将全局的 Promise 变量重写为自定义的 Polyfill，如果你不需要兼容 iOS10 也可以使用以下方式：
-     * Promise = Object.getPrototypeOf((async () => {})()).constructor;
-     * 写在此处是为了保证 Promise 重写最先被执行。
-     */
-    code = code.replace(
-      '"use strict";',
-      '"use strict";\n\nvar PromisePolyfill = require("promise-polyfill");\nPromise = PromisePolyfill.default;',
-    );
-    const promise = bundleModule('promise-polyfill');
-    bundleJobs?.push(promise);
-  }
-
   traverseAST(ast);
 
   if (__PROD__) {
